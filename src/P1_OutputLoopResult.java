07ab33ec570a61a852b5024873c5b80dfade8454
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-04 00:58:39 -0500

    Shifted variable declaration out of for loop

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -1,6 +1,6 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Anthony Quehl
   	Date: 
 */
 
@@ -9,16 +9,16 @@
 public class P1_OutputLoopResult {
 
 	public static void main(String[] args) {
+		
 		printSumOfSquaresTo5();
 	}
 	
 	public static void printSumOfSquaresTo5() {
+		int finalResult = 0;
 		for( int i = 1; i <= 5; i++ ) {
-			int finalResult = 0;
-			finalResult = finalResult + i * i;
-		}
-		
-		System.out.println( "Final result is: " + finalResult );
+			finalResult = finalResult + (i * i);			
+		}		
+		System.out.println( "Final result is: " + finalResult  );
 	}
 
 }
